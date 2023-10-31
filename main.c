12e3b67e1c753db97cbbb5609ab635c9556296ce
imad.exe <imadidaliouali@gmail.com>
2023-10-31 22:41:53 +0100

    Selection sort and Insertion sort done

--- main.c
+++ main.c
@@ -1,6 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void selectionSort(int arr[], int n)
+{
+    int i, j, min_idx;
+    for (i = 0; i < n - 1; i++)
+    {
+        min_idx = i;
+        for (j = i + 1; j < n; j++)
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+        if (min_idx != i)
+            swap(&arr[min_idx], &arr[i]);
+    }
+}
+
+void insertionSort(int arr[], int n)
+{
+    int i, key, j;
+    for (i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void printArray(int *arr, int n)
+{
+    int i;
+    printf("Arr = (");
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf(")\n");
+}
 
 int main()
 {
+    int arr[5] = {2, 4, 1, 5, 3};
+    printArray(arr, 5);
+    insertionSort(arr, 5);
+    printArray(arr, 5);
     return 0;
 }
