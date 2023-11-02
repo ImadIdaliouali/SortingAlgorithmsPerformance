5ee738e51c65b391593bbb7bc9c609e178faad20
imad.exe <imadidaliouali@gmail.com>
2023-11-02 20:38:26 +0100

    Add utils.h with array and timing utility functions

---
+++ utils.h
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int *generateRandomArray(int n)
+{
+    int *arr = (int *)malloc(n * sizeof(int));
+    int i;
+    for (i = 0; i < n; i++)
+        arr[i] = rand();
+    return arr;
+}
+
+double timeTaken(int *arr, int n, void fun(int *, int))
+{
+    time_t start, end;
+    double execTime;
+    start = clock();
+    fun(arr, n);
+    end = clock();
+    return (double)(end - start) / CLOCKS_PER_SEC;
+}
+
+void printMatrix(double mat[][3], int n)
+{
+    int i;
+    printf("Size, Selection Sort, Insertion Sort\n");
+    for (i = 0; i < n; i++)
+        printf("%.0f, %f, %f\n", mat[i][0], mat[i][1], mat[i][2]);
+}
+
+int *copyArray(int *arr, int n)
+{
+    int *cArr = (int *)malloc(n * sizeof(int));
+    int i;
+    for (i = 0; i < n; i++)
+        cArr[i] = arr[i];
+    return cArr;
+}
